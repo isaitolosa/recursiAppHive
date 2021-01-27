79bf7a3e1011544ab7f1da8fbc370f1941511ab6
Isai Tolosa Avila <itolosaa@toluca.tecnm.mx>
2021-01-27 00:50:13 -0600

    La función jsonToExcel ya funciona

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const ExcelJS = require("exceljs");
 
-let archivoRAW = fs.readFileSync(__dirname + "/pruebas/isaijsonexample.json");
+let archivoRAW = fs.readFileSync(__dirname + "/pruebas/jsonGigante.json");
 let archivoJSON = JSON.parse(archivoRAW);
 const workbook = new ExcelJS.Workbook();
 workbook.creator = "IsaiT";
@@ -20,7 +20,6 @@ function jsonToExcel(
   reiniciarLevel,
   filaInser
 ) {
-  console.log("Se llamo la funcion_____________________________");
   if (reiniciarLevel === "si") {
     level = 2;
   } else {
@@ -42,15 +41,6 @@ function jsonToExcel(
     if (typeof objeto[collection] !== "object") {
       sheet = workbook.getWorksheet(abuelo);
 
-      console.log(
-        "Los valores son: " +
-          collection +
-          ": " +
-          objeto[collection] +
-          ". En la página: " +
-          abuelo +
-          "."
-      );
       //sheet.addRow([3, "Sam", new Date()]);
 
       let esPrinOsec = abuelo.search("-");
@@ -72,7 +62,6 @@ function jsonToExcel(
 
         fila.values = encabezados;
         fila = sheet.getRow(1);
-        console.log(fila.values);
 
         //console.log(fila.values);
 
@@ -84,7 +73,6 @@ function jsonToExcel(
             numeroCol = rowNum;
           }
         });
-        console.log(numeroCol);
         fila = sheet.getRow(1);
 
         let last = sheet.getColumn(1);
@@ -94,10 +82,6 @@ function jsonToExcel(
         });
 
         let filaTemp;
-        console.log(sheet.getRow(1).values);
-        console.log(sheet.getRow(2).values);
-        console.log(sheet.getRow(3).values);
-        console.log(sheet.getRow(4).values);
 
         if (ultimaFila === 1) {
           ultimaFila = ultimaFila + 1;
@@ -105,8 +89,8 @@ function jsonToExcel(
         } else {
           filaTemp = sheet.getRow(ultimaFila).values[1];
         }
-        console.log(filaTemp);
-        if (filaTemp !== padre) {
+        if (filaTemp.length === 0) {
+        } else if (filaTemp !== padre) {
           ultimaFila = ultimaFila + 1;
         }
 
@@ -114,7 +98,6 @@ function jsonToExcel(
         //Insertar id
         cell = sheet.getCell(ultimaFila, 1);
         cell.value = padre;
-        console.log("El ID: " + collection + " va en:" + cell.address);
 
         fila = sheet.getRow(1);
         //Insertar celda
@@ -123,28 +106,75 @@ function jsonToExcel(
 
         fila = sheet.getRow(1);
 
-        console.log(
-          "La collection: " + objeto[collection] + " va en:" + cell.address
-        );
-
         fila = sheet.getRow(1);
-        console.log("________________________________________________________");
 
         cell = null;
       } else {
         //Es hoja anidada
         let fila = sheet.getRow(1);
-        let encabezados = [];
+
         let celda;
-        if (fila == null || !fila.values || !fila.values.length) {
-          celda = sheet.getCell(1, 3);
-          celda.value = collection;
+        encabezados = fila.values;
+        fila = sheet.getRow(1);
+        if (fila === null || !fila.values || !fila.values.length) {
+          encabezados.push("");
+          encabezados.push("");
+          encabezados.push(collection);
         } else {
-          for (let i = 1; i < fila.values.length; i++) {
-            let cell = fila.getCell(i).text;
-            encabezados.push(cell);
+          if (!encabezados.includes(collection)) {
+            encabezados.push(collection);
           }
         }
+
+        fila.values = encabezados;
+        fila = sheet.getRow(1);
+
+        //Buscar nombre de columna en la fila y sacar su posicion
+
+        let numeroCol;
+        fila.eachCell(function (cel, rowNum) {
+          if (cel.text === collection) {
+            numeroCol = rowNum;
+          }
+        });
+        fila = sheet.getRow(1);
+
+        let last = sheet.getColumn(1);
+        let ultimaFila;
+        last.eachCell(function (cel, rowNum) {
+          ultimaFila = rowNum;
+        });
+
+        let filaTemp;
+
+        if (ultimaFila === 1) {
+          ultimaFila = ultimaFila + 1;
+          filaTemp = sheet.getRow(ultimaFila).values;
+        } else {
+          filaTemp = sheet.getRow(ultimaFila).values[2];
+        }
+        if (filaTemp.length === 0) {
+        } else if (filaTemp !== padre) {
+          ultimaFila = ultimaFila + 1;
+        }
+
+        fila = sheet.getRow(1);
+        //Insertar ids
+        cell = sheet.getCell(ultimaFila, 1);
+        cell.value = bisAbuelo;
+        cell = sheet.getCell(ultimaFila, 2);
+        cell.value = padre;
+
+        fila = sheet.getRow(1);
+        //Insertar celda
+        cell = sheet.getCell(ultimaFila, numeroCol);
+        cell.value = objeto[collection];
+
+        fila = sheet.getRow(1);
+
+        fila = sheet.getRow(1);
+
+        cell = null;
       }
     } else {
       if (level === 1) {
@@ -183,7 +213,6 @@ function jsonToExcel(
       }
     }
   }
-  console.log("TERMINAMOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOS");
   reiniciarLevel = "no";
 }
 
